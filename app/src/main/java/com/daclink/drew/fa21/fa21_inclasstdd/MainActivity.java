6690078d2b1b4f3087d980814a6ab827eb528ad9
Dr.C <dclinkenbeard@csumb.edu>
2021-08-26 10:30:51 -0700

    Intial commit.

---
+++ app/src/main/java/com/daclink/drew/fa21/fa21_inclasstdd/MainActivity.java
@@ -0,0 +1,14 @@
+package com.daclink.drew.fa21.fa21_inclasstdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
