596e6fb22440aeb443569a8a1aa4dab9c779aacd
Dr.C <dclinkenbeard@csumb.edu>
2021-08-26 10:47:49 -0700

    Added multiplication thing including tests. which pass. yay.

---
+++ app/src/main/java/com/daclink/drew/fa21/fa21_inclasstdd/Util.java
@@ -0,0 +1,7 @@
+package com.daclink.drew.fa21.fa21_inclasstdd;
+
+public class Util {
+    public static int multiply(int x, int y){
+        return x*y;
+    }
+}
--- app/src/test/java/com/daclink/drew/fa21/fa21_inclasstdd/ExampleUnitTest.java
+++ app/src/test/java/com/daclink/drew/fa21/fa21_inclasstdd/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void multiplication_test(){
+        Random random = new Random();
+        int x = random.nextInt(1000);
+        int y = random.nextInt(1000);
+
+        assertEquals(x*y, Util.multiply(x,y));
     }
 }
